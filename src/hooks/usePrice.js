008019560b6c75d1f4bf211e9ba8cdf10a3848cb
HadesHappy <happyhades123@gmail.com>
2023-03-13 18:43:39 +0900

    coingecko

--- src/hooks/usePrice.js
+++ src/hooks/usePrice.js
@@ -1,33 +1,23 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useStateInfo } from './useStateInfo'
-import CoinGecko from 'coingecko-api'
 
 export const usePrice = () => {
   const [ethPrice, setEthPrice] = useState()
   const { inputValue, outputValue } = useStateInfo()
-  const CoinGeckoClient = new CoinGecko()
 
-  useEffect(async() => {
+  useEffect(() => {
     axios.get("https://api.binance.com/api/v3/ticker/price?symbol=ETHBUSD")
       .then((res) => {
-        if(res.data){
+        if (res.data) {
           setEthPrice(res.data.price)
-        }else{
+        } else {
           console.log('no data')
         }
       })
       .catch((error) => {
         console.log(error)
       })
-
-      let data = await CoinGeckoClient.simple.price({
-        ids: ['bitcoin', 'ethereum'],
-        vs_currencies: 'usd'
-      })
-
-      console.log('data: ', data)
-
   }, [inputValue, outputValue])
 
   return { ethPrice }
